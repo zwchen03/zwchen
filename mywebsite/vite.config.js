f59e054c822ddf28436bddcee1baa98e7b0483aa
helloworldczw <1561369591@qq.com>
2025-03-17 17:21:10 +0800

    fix bug

--- mywebsite/vite.config.js
+++ mywebsite/vite.config.js
@@ -17,4 +17,8 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  build: {
+    outDir: 'public' // 与CI配置一致
+  },
+  publicDir: 'assets'
 })
