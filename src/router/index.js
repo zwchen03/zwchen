e03142205cd66a22d4268ec359f62c8112751ca3
helloworldczw <1561369591@qq.com>
2025-06-15 17:22:25 +0800

    修改路由模式

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../components/HomePage.vue'
 // 使用懒加载导入演示组件
 const LazyLoadingDemo = () => import('../components/LazyLoadingDemo.vue')
 
 const router = createRouter({
   // history: createWebHistory(import.meta.env.BASE_URL),
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
